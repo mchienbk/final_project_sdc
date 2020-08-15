2462fec5bad604ba3783cef94ae0dbd8cd05d262
Tran Minh Chien <minhchien2031995@gmail.com>
2020-08-16 01:37:30 +0700

    visual odometry

--- my_params.py
+++ my_params.py
@@ -1,26 +1,27 @@
+# Primary patch
+dataset_no = '20151030'
+project_patch = 'D:\\Github\\final_project_sdc\\'
+dataset_patch = 'D:\\Dataset\\' + dataset_no + '\\'
+# dataset_patch = "D:\\Dataset\\20140514\\"
 
-
-# project_patch = 'D:\\Github\\final_project_sdc\\'
-# dataset_patch = 'D:\\Dataset\\20151030\\'
-
-project_patch = "D:\\Github\\final_project_sdc\\"
-dataset_patch = "D:\\Dataset\\20140514\\"
-
-
-poses_file = dataset_patch + 'vo\\vo.csv'
-extrinsics_dir = project_patch + 'extrinsics'
-laser_dir = dataset_patch + 'ldmrs'
-
+# camera
 camera = 'stereo\\centre'
 camera_name = 'stereo'
 
+# Folder directory
 image_dir = dataset_patch + camera
+laser_dir = dataset_patch + 'ldmrs'
 model_dir = project_patch + 'models'
-
-model = model_dir + camera_name
-
+extrinsics_dir = project_patch + 'extrinsics'
 reprocess_image_dir = image_dir + '_processed'
+output_dir = project_patch + 'output'
 
+
+# Data file directory
+model = model_dir + camera_name
+poses_file = dataset_patch + 'vo\\vo.csv'
+
+# Yolo data directory
 yolo_cfg = project_patch + 'yolo\\cfg\\yolov3.cfg'
 yolo_weights = project_patch + 'yolo\weights\yolov3.weights'
 yolo_data = project_patch +'yolo\\data\\'
